06e8311466a802020685d257e3196855ae4e155a
FrejaIBredahl <201904700@post.au.dk>
2022-04-21 13:21:19 +0200

    Connected to firebase

--- app/src/main/java/dk/au/mad22spring/appproject/group7/MainActivity.java
+++ app/src/main/java/dk/au/mad22spring/appproject/group7/MainActivity.java
@@ -2,13 +2,30 @@ package dk.au.mad22spring.appproject.group7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        storageRef = FirebaseStorage.getInstance().getReference();
     }
+
+
 }
