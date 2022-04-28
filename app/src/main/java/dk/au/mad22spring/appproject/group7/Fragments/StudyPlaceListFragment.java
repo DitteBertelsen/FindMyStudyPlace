45a2571769c455cbfaab7f21661c9057a6f3f8a0
Ditte Bertelsen <201907172@uni.au.dk>
2022-04-28 11:46:13 +0200

    Updated UI for recycler view to cards

--- app/src/main/java/dk/au/mad22spring/appproject/group7/Fragments/StudyPlaceListFragment.java
+++ app/src/main/java/dk/au/mad22spring/appproject/group7/Fragments/StudyPlaceListFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -54,6 +55,7 @@ public class StudyPlaceListFragment extends Fragment implements StudyPlaceListVi
 
         adapter = new StudyPlaceListViewAdaptor(this);
         layoutMan = new LinearLayoutManager(getContext());
+
         rcvStudyPlaceView.setAdapter(adapter);
         rcvStudyPlaceView.setLayoutManager(layoutMan);
 
