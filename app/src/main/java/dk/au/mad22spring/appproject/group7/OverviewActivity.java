756fa5466d1586a778ef541811af9b1cb5d0d0df
trinekmadsen <201907803@post.au.dk>
2022-04-28 12:11:04 +0200

    LatLongs added to StudyPlace model and to tempStudyPlaces

--- app/src/main/java/dk/au/mad22spring/appproject/group7/OverviewActivity.java
+++ app/src/main/java/dk/au/mad22spring/appproject/group7/OverviewActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
